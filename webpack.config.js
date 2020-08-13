3ee19d2a6135a139f11393dbe9ef00fba967faaf
guobgf <guobgf@yonyou.com>
2020-08-13 16:15:59 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.join(__dirname, '..', 'src')
+        },
+        extensions: [".ts", ".tsx", ".js", ".vue"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                }
+            },
+            {
+                test: /\.vue$/,
+                use: ['vue-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),   // dist目录开启服务器
+        compress: true,    // 是否使用gzip压缩
+        port: 9000,    // 端口号
+        open: true   // 自动打开网页
+    }
+}
